25e371ad5872e3a1e53472af125c8c345c1ec12e
GolDCipolin <123726815+GolDCipolin@users.noreply.github.com>
2026-02-04 21:25:17 +0000

    pokemon randomized in refresh
    stored pokemon information to show the name, image source, the catch percent in the database. Every time you refresh the page it randomly picks a pokemon

--- pages/main.php
+++ pages/main.php
@@ -1,3 +1,10 @@
+<?php
+require_once("../require_once/dp.php");
+
+$stmd = $pdo->prepare("SELECT * FROM pokemon ORDER BY RAND() LIMIT 1");
+$stmd->execute();
+$pokemon = $stmd->fetch(PDO::FETCH_ASSOC);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,17 +14,21 @@
     <link rel="stylesheet" href="/css/base.css">
 </head>
 <div class="wrapper">
-<body>
-    <?php require_once("../require_once/header.php"); ?>
+    <body>
+        <?php require_once("../require_once/header.php"); ?>
 
 
 
-    <h1 class="title">WebMons</h1>
-    <img src="/images/pikachu.png" alt="Pikachu" width="200" height="200" class="center-image">
-    <button type="button">Catch</button>
-    <button type="button">Bait</button>
-    <button type="button">Rock</button>
-    <button type="button">Run</button>
-</body>
+        <h1 class="title">WebMons</h1>
+        <h2><?= $pokemon['pokeName'] ?></h2>
+        <img src="<?= $pokemon['imageSrc'] ?>" alt="<?= $pokemon['pokeName'] ?>" width="200" height="200" class="center-image">
+        <!-- <img src="/images/pikachu.png" alt="Pikachu" width="200" height="200" class="center-image"> -->
+         <p>Catch chance: <?= $pokemon['baseCatchPercent'] ?>%</p>
+        <button type="button">Catch</button>
+        <button type="button">Bait</button>
+        <button type="button">Rock</button>
+        <button type="button">Run</button>
+        
+    </body>
 </div>
 </html>
